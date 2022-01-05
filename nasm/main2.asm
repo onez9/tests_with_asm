03bf6f9a99b205022a0f8476a33b58805d95e9b7
z9one <ar.z.v@yandex.ru>
2022-01-06 00:56:29 +0500

    my first commit

--- nasm/main2.asm
+++ nasm/main2.asm
@@ -1,16 +1,19 @@
 
 section	.data
-	msg db 'D',0xa ; наше сообщение
+	msg db 'Аниме это хорошо',0xa ; наше сообщение
 	len equ $- msg  ; длина нашего сообщения 
 	;m2 times 9 dw 0
 	;n1 db '1'
 	;n2 db '1'
-	n1 db 1
+	n1 db 7
 	n2 db 1
+	count dw 0
+	msg2 db 'Type value: ', 0xa
+	len2 equ $- msg2
 
 segment .bss
-	num1 resb 2
-	num2 resb 2
+	num1 resb 3
+	num2 resb 3
 
 
 section	.text
@@ -38,8 +41,22 @@ _start:	       ; сообщаем линкеру точку входа
 	mov eax, 4
 	mov ebx, 1
 	mov ecx, num1
-	mov edx, 1
+	mov edx, 5
 	int 0x80
+
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, msg2
+	mov edx, len2
+	int 0x80
+
+	mov eax, 3
+	mov ebx, 0
+	mov ecx, num2
+	mov edx, 3
+	int 0x80
+
+
 	
   mov	eax,1    ; номер системного вызова (sys_exit)
   int	0x80     ; вызов ядра
